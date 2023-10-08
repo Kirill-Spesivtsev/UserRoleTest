b94b9c9b78f27faeb6ac8907c47b637591817688
Kirill-Spesivtsev <ksby8819@gmail.com>
2023-10-08 15:39:19 +0300

    Set up project, added models

---
+++ UserRoleTest/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace UserRoleTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
---
+++ UserRoleTest/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRoleTest.Models
+{
+    public class Role
+    {
+        public Guid Id {get; set;}
+
+        public string Name {get; set;}
+
+        public List<Role> Users {get; set;} = new();
+
+    }
+}
